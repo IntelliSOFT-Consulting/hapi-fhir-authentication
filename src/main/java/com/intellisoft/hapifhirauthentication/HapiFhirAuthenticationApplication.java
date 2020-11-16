b796859329cd58f79b7d9ef442bba5366a2030a5
corneliouzbett <kbett68@gmail.com>
2020-11-16 11:06:48 +0300

    Add swagger2 documentation

--- src/main/java/com/intellisoft/hapifhirauthentication/HapiFhirAuthenticationApplication.java
+++ src/main/java/com/intellisoft/hapifhirauthentication/HapiFhirAuthenticationApplication.java
@@ -1,9 +1,11 @@
 package com.intellisoft.hapifhirauthentication;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class HapiFhirAuthenticationApplication {
 
 	public static void main(String[] args) {
