15291be62c46c8cc43032a141c90c7f5fd46e9e3
corneliouzbett <kbett68@gmail.com>
2020-11-16 09:56:03 +0300

    Initial hapi-fhir-authentication commit

---
+++ src/main/java/com/intellisoft/hapifhirauthentication/HapiFhirAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.intellisoft.hapifhirauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HapiFhirAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HapiFhirAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intellisoft/hapifhirauthentication/HapiFhirAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intellisoft.hapifhirauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HapiFhirAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
