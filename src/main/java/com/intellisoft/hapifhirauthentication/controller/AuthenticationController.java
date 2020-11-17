af9d7d607172dae314d9b12819503459c0fe9198
corneliouzbett <kbett68@gmail.com>
2020-11-17 16:27:35 +0300

    Provide operation description for rest endpoints

--- src/main/java/com/intellisoft/hapifhirauthentication/controller/AuthenticationController.java
+++ src/main/java/com/intellisoft/hapifhirauthentication/controller/AuthenticationController.java
@@ -4,6 +4,7 @@ import com.intellisoft.hapifhirauthentication.domains.AccessToken;
 import com.intellisoft.hapifhirauthentication.domains.KeycloakUser;
 import com.intellisoft.hapifhirauthentication.exceptions.NotAuthorizedException;
 import com.intellisoft.hapifhirauthentication.service.AuthenticationService;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -16,14 +17,14 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationService authenticationService;
 
-    //@ApiOperation(value = "Give OAuth access token given user and password")
+    @ApiOperation(value = "Give OAuth access token given user and password")
     @PostMapping("/login")
     public AccessToken login(@RequestBody KeycloakUser user) throws NotAuthorizedException {
         log.info("Login user");
         return authenticationService.login(user);
     }
 
-    //@ApiOperation(value = "Provide the user preferred name given the token")
+    @ApiOperation(value = "Provide the user preferred name given the token")
     @GetMapping("/user")
     public String user(@RequestHeader(HttpHeaders.AUTHORIZATION) String token) throws NotAuthorizedException {
         log.info("Get User info");
